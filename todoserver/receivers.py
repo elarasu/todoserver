5b3ea737be51708070af386c1ddae1e44b29bfb2
el arasu <elarasu@outlook.com>
2015-08-21 15:42:09 +0000

    db events

--- todoserver/receivers.py
+++ todoserver/receivers.py
@@ -6,7 +6,6 @@ from account.signals import user_login_attempt, user_logged_in
 
 from eventlog.models import log
 
-
 @receiver(user_logged_in)
 def handle_user_logged_in(sender, **kwargs):
     log(
@@ -57,3 +56,31 @@ def handle_user_signed_up(sender, **kwargs):
         action="USER_SIGNED_UP",
         extra={}
     )
+
+from django.db.models.signals import post_save, post_delete, m2m_changed
+from todo.models import TodoTask
+from todo.serializers import TodoSerializer
+import json
+
+#{'update_fields': None, 'instance': <TodoTask: 2 a 0>, 'signal': <django.db.models.signals.ModelSignal object at 0x7f7acbdfe4d0>, 'created': False, 'raw': False, 'using': 'default'}
+@receiver(post_save, sender=TodoTask)
+def my_handler(sender, **kwargs):
+    obj = kwargs.get("instance")
+    res = {}
+    res['id'] = obj.id
+    res['object'] = TodoSerializer(obj).data
+    if kwargs.get("created"):
+        res['event']='add'
+    else:
+        res['event']='set'
+    print json.dumps(res)
+
+#{'instance': <TodoTask: 2 a 0>, 'signal': <django.db.models.signals.ModelSignal object at 0x7f7acbdfe5d0>, 'using': 'default'}
+@receiver(post_delete, sender=TodoTask)
+def my_delete_handler(sender, **kwargs):
+    obj = kwargs.get("instance")
+    res = {}
+    res['id'] = obj.id
+    res['event'] = 'remove'
+    print json.dumps(res)
+
